f8a44e48b530b7f4e29b06ecad35b8bb9811ac44
Pablo Rodriguez <pablor@bombieri.com.ar>
2022-03-21 21:28:33 -0300

    global styles and module for prime ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, PrimeNgModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
