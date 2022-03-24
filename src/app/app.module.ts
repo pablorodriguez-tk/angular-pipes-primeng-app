5d43ae3d17dce53e285fdfd52d9ec855800b9e98
Pablo Rodriguez <pablor@bombieri.com.ar>
2022-03-24 19:18:31 -0300

    basic pipes and global lenguage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
+//Cambiar el locale de la app
+import localeEs from '@angular/common/locales/es-AR';
+import { registerLocaleData } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, AppRouterModule],
-  providers: [],
+  imports: [BrowserModule, SharedModule, AppRouterModule, VentasModule],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-AR',
+    },
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    registerLocaleData(localeEs);
+  }
+}
--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
+  nombreLower: string = 'pablo';
+  nombreUpper: string = 'PABLO';
+  nombreCompleto: string = 'PaBlO RoDrIguEZ';
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  fecha: Date = new Date();
 }
