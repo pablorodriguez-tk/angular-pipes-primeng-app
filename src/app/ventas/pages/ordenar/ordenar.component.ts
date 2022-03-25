a6cf0791ad8e314923987055186ef60eedabb565
Pablo Rodriguez <pablor@bombieri.com.ar>
2022-03-24 23:54:52 -0300

    custom pipe - uppercase

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
+  enMayusculas: boolean = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  cambiar() {
+    this.enMayusculas = !this.enMayusculas;
   }
-
 }
--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -5,13 +5,14 @@ import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { BasicosComponent } from './pages/basicos/basicos.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
-
+import { MayusculasPipe } from './pipes/mayusculas.pipe';
 @NgModule({
   declarations: [
     NumerosComponent,
     NoComunesComponent,
     BasicosComponent,
     OrdenarComponent,
+    MayusculasPipe,
   ],
   exports: [
     NumerosComponent,
