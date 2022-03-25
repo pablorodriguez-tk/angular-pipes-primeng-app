93e2f88c8482944e1e628b4031973cca97d9721f
Pablo Rodriguez <pablor@bombieri.com.ar>
2022-03-24 23:27:47 -0300

    async pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -42,4 +43,12 @@ export class NoComunesComponent {
     { nombre: 'Flash', poder: 'Velocidad' },
     { nombre: 'Aquaman', poder: 'Volar', ojos: true },
   ];
+
+  //Async Pipe
+  miObservable = interval(1000); // 0 , 1 , 2 , 3 , 4 .....
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data de promesa');
+    }, 3500);
+  });
 }
